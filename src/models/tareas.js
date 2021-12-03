a41f82771d828a9d4a8ea2e896647497f0520da5
abdieljz <55746600+abdieljz@users.noreply.github.com>
2021-12-03 00:48:12 -0500

    Klar

--- src/models/tareas.js
+++ src/models/tareas.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-//como vana  lucir los daots
+//como vana  lucir los datos en la base de datos
 const Schema = mongoose.Schema;
 
 const TaskSchema  = new Schema({
